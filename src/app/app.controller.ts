067d623280686d9cb889a4ac1338c707e2931000
Argy <hello@arggeo.com>
2023-04-24 20:19:58 +0300

    Initial commit

---
+++ src/app/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GenericObject } from '../interfaces/generics';
+
+@Controller()
+export class AppController {
+   constructor(private readonly appService: AppService) {}
+
+   @Get()
+   getNothingToSeeHere(): string {
+      return this.appService.getNothingToSeeHere();
+   }
+
+   @Get('info')
+   getInfo(): GenericObject {
+      return this.appService.getInfo();
+   }
+}
---
+++ src/app/app.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { GenericObject } from '../interfaces/generics';
+
+@Injectable()
+export class AppService {
+   getNothingToSeeHere(): string {
+      return 'Nothing to see here. Access "/info" to view credits.';
+   }
+
+   getInfo(): GenericObject {
+      return {
+         project: 'NestJS Blog Base',
+         githubUrl: '',
+         author: 'Argy Georgiadis',
+         authorEmail: 'hello@arggeo.com',
+      };
+   }
+}
